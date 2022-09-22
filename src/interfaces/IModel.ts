c902e36bda93f64fee094489e9a33e4dc370f3d9
deyvid-dev <deyvidscm@hotmail.com>
2022-09-22 00:16:51 -0300

    req 01

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,9 +1,15 @@
-interface IModel<T> {
-  create(obj: T): Promise<T>
-  read(): Promise<T>
-  readOne(_id: string): Promise<T | null>
-  update(_id: string): Promise<T | null>
-  delete(_id: string): Promise<T | null>
+export interface IModel<T> {
+  create(obj: T): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(_id: string): Promise<T | null>,
+  update(_id: string, obj: T): Promise<T | null>,
+  delete(_id: string): Promise<T | null>,
 }
 
-export default IModel;
+// export interface IModel<T> {
+//   create(obj:T):Promise<T>,
+//   read():Promise<T[]>,
+//   readOne(_id:string):Promise<T | null>,
+//   update(_id:string, obj:T):Promise<T | null>,
+//   delete(_id:string):Promise<T | null>,
+// }
