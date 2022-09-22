9ab30d881066d2f545eb7bc8b3198bde3cec4129
deyvid-dev <deyvidscm@hotmail.com>
2022-09-22 00:19:05 -0300

    iniciando req2

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -5,11 +5,3 @@ export interface IModel<T> {
   update(_id: string, obj: T): Promise<T | null>,
   delete(_id: string): Promise<T | null>,
 }
-
-// export interface IModel<T> {
-//   create(obj:T):Promise<T>,
-//   read():Promise<T[]>,
-//   readOne(_id:string):Promise<T | null>,
-//   update(_id:string, obj:T):Promise<T | null>,
-//   delete(_id:string):Promise<T | null>,
-// }
