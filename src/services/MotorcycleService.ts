c6c29bce7e5fef2980a2385862b5e003a299773c
deyvid-dev <deyvidscm@hotmail.com>
2022-10-05 23:57:46 -0300

    req-19

--- src/services/MotorcycleService.ts
+++ src/services/MotorcycleService.ts
@@ -14,8 +14,33 @@ class MotorcycleService implements IService<IMotorcycle> {
     if (!parsed.success) {
       throw parsed.error;
     }
-
     return this._motorclycle.create(obj);
+  }
+
+  public async read(): Promise<IMotorcycle[]> {
+    const result = await this._motorclycle.read();
+    if (!result) throw new Error('Error');
+    return result;
+  }
+
+  public async readOne(_id: string): Promise<IMotorcycle | null> {
+    const result = await this._motorclycle.readOne(_id);
+    return result;
+  }
+
+  public async delete(_id: string): Promise<IMotorcycle | null> {
+    const result = await this._motorclycle.delete(_id);
+    return result;
+  }
+
+  public async update(_id: string, obj: IMotorcycle): Promise<IMotorcycle | null> {
+    const result = await this._motorclycle.update(_id, obj);
+    const parsed = motoSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return result;
   }
 }
 
